d61f718333f8dae3e44797f572f7e90df2e47247
gega0397 <g_kandelaki@cu.edu.ge>
2024-04-15 17:43:21 +0400

    updated admin page

--- market/admin.py
+++ market/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from market.models import Book,Author,Category
+from market.models import Book, Author, Category
 
 
 # Register your models here.
@@ -9,19 +9,23 @@ from market.models import Book,Author,Category
 
 @admin.register(Book)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'page_count', 'price', 'display_authors', 'display_categories')
+    list_display = ('id', 'name', 'page_count', 'price', 'display_authors', 'display_categories', 'cover')
 
     def display_authors(self, obj):
         return ', '.join([author.name + ' ' + author.surname for author in obj.authors.all()])
+
     display_authors.short_description = 'Authors'
 
     def display_categories(self, obj):
         return ', '.join([category.genre for category in obj.categories.all()])
+
     display_categories.short_description = 'Categories'
+
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name', 'surname', 'birth_date')
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
