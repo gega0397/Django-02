922e61f09308ea6cf6f9eb874ae31ce6d19746b8
gega0397 <g_kandelaki@cu.edu.ge>
2024-04-15 20:14:01 +0400

    updated urls

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path("admin/", admin.site.urls),
     path('', include('market.urls', namespace='market')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
